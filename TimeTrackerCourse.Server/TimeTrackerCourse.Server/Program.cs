5a73ff64143c3d1edc3cd60bc9704c048785c173
Ove Ronny Bolme <overonny@progorb.no>
2024-05-12 14:21:07 +0200

    updated app

--- TimeTrackerCourse.Server/TimeTrackerCourse.Server/Program.cs
+++ TimeTrackerCourse.Server/TimeTrackerCourse.Server/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddServiceLayer();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
 }
